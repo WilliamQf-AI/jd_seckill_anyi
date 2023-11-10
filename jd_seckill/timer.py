8c89c87da6e60099e23fbd97b48761a7dd41c4ec
WilliamQf-AI <r187@foxmail.com>
2023-11-10 15:54:21 +0800

    修改优化后的代码

--- jd_seckill/timer.py
+++ jd_seckill/timer.py
@@ -25,7 +25,7 @@ class Timer(object):
         self.buy_time_ms = int(time.mktime(self.buy_time.timetuple()) * 1000.0 + self.buy_time.microsecond / 1000)
         self.sleep_interval = sleep_interval
 
-        self.diff_time = self.local_jd_time_diff()
+        #self.diff_time = self.local_jd_time_diff()
 
     def jd_time(self):
         """
@@ -49,14 +49,22 @@ class Timer(object):
         计算本地与京东服务器时间差
         :return:
         """
-        return self.local_time() - self.jd_time()
+        val = self.local_time() - self.jd_time()
+        return val
 
     def start(self):
-        logger.info('正在等待到达设定时间:{}，检测本地时间与京东服务器时间误差为【{}】毫秒'.format(self.buy_time, self.diff_time))
+        logger.info('正在等待到达设定时间:{}'.format(self.buy_time))
+        #logger.info('正在等待到达设定时间:{}，检测本地时间与京东服务器时间误差为【{}】毫秒'.format(self.buy_time, self.diff_time))
         while True:
             # 本地时间减去与京东的时间差，能够将时间误差提升到0.1秒附近
             # 具体精度依赖获取京东服务器时间的网络时间损耗
-            if self.local_time() - self.diff_time >= self.buy_time_ms:
+            #if self.local_time() - self.diff_time >= self.buy_time_ms:
+            now_time = datetime.now
+            # 将日期和时间转换为时间元组  
+            current_time_tuple = now_time().timestamp()  
+            # 将时间元组转换为毫秒数  
+            current_timestamp_milliseconds = int(current_time_tuple * 1000) 
+            if current_timestamp_milliseconds >= self.buy_time_ms:
                 logger.info('时间到达，开始执行……')
                 break
             else:
